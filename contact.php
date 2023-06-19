f9d22d6a4c9cf19e3d7c6aaf1d5328244f215ae0
Ankit <pingmehereitsankit@gmail.com>
2023-06-18 23:46:44 -0700

    added my name

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
-    echo "Contact me at pingmehereitsankit@gmail.com";
-    echo 'contact me at 9756120758';
+    echo "Contact me at pingmehereitsankit@gmail.<br/>";
+    echo 'contact me at 9756120758<br/>';
+    echo 'my name is ankit';
 ?>
