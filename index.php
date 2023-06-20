cbb2c805199936729d6eb3fe3a20ea37d9620702
Ankit <pingmehereitsankit@gmail.com>
2023-06-20 13:01:10 +0530

    made changes in few files

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
     echo "Digging deep into git/scm<br/>";
     echo "Editing file to check git working procedure.<br/>";
     echo "Editing file from browser.";
+    echo "<br/>";
+    for($i=1;$i<=10;$i++){
+        echo (2*$i)."<br/>";
+    }
 ?>
--- singh/text.php
+++ singh/text.php
@@ -3,6 +3,6 @@ echo "hello<br/>,";
 echo "ankit<br/> ";
 echo "Singh<br/>";
 echo 'my name is ankit<br/>';
-echo 'i am very busy person';
+echo 'i am very busy person<br/>';
 echo 'i am not';
 ?>
